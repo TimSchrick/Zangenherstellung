0eda670958ab3138616ce58e70d1f3980d8f49e2
marcelarmin <marcel.armin@web.de>
2020-03-18 15:44:56 +0100

    Test funktion nach "aaa" aufgebaut

--- Test_btv.py
+++ Test_btv.py
@@ -1,57 +1,20 @@
-"""
-IMPORT:
-class Zange:
-    def __init__(self):
-        self.erhitzt = False
-        self. gebogen = False
-        self.geschmiedet = False
-        self.abgegratet = False
-        self.getellert = False
-        self.versenkt = False
-        self.gebohrt = False
-        self.geoelt = False
-        self.vormontiert = False
-        self.vernietet = False
-        self.geoeffnet = False
-        self.eingestellt = False
-        self.geprueft = False
-"""
 import btv
 from Zange import Zange
 
-def Test_anlieferung(schenkel:Zange):
-    """
-    Hiermit teste ich den Anlieferungszustand für die Maschine/Funktion "btv"
-    :param schenkel:
-    :return: True --> anliefrungszusatnd richtig
-    False --> Eins der Merkmale die hier bearbeitet wird ist nicht richtig oder schon bearbeitet
-    """
+def test_btv():
+    "arrange:"
+    schenkel = Zange()
+    testergebnis = False
+
+    "act:"
+    # Hiermit teste ich den Anlieferungszustand für die Maschine/Funktion "btv"
     if schenkel.getellert == False and schenkel.versenkt == False and schenkel.gebohrt == False:
-        return True
-    else:
-        return False
+        # Zange bearbeiten
+        schenkel = btv.BTV_bearbeitung(schenkel)
+        # Hiermit teste ich die Funktion (die Maschine) "btv"
+        if schenkel.getellert == True and schenkel.versenkt == True and schenkel.gebohrt == True:
+            testergebnis = True
 
-def Test_auslieferung(schenkel:Zange):
-    """
-    Hiermit teste ich die Funktion (die Maschine) "btv"
-    :param schenkel:
-    :return: True --> Auslieferung richtig
-    False --> Auslieferung falsch
-    """
-    if schenkel.getellert == True and schenkel.versenkt == True and schenkel.gebohrt == True:
-        return True
-    else:
-        return False
-def Test_btv():
-    teil = Zange()
+    "assert:"
+    assert testergebnis
 
-    if Test_anlieferung(teil) == True:
-        teil = btv.BTV_bearbeitung(teil)
-        if Test_auslieferung(teil) == True:
-            return True
-    else:
-        return False
-
-# ------btv_test--------
-if Test_btv() == True:
-    print("btv funktioniert")
