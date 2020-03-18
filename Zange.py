76a7b4cfbc430da315aeb1742dd04bc7e18a9419
LMIS\HEW <Henning.Wuebben@LMIS.de>
2020-03-18 13:17:57 +0100

    hier, ne Zange

---
+++ Zange.py
@@ -0,0 +1,17 @@
+# Created by HEW at 18.03.2020
+
+class Zange:
+    def __init__(self):
+        self.erhitzt = False
+        self. gebogen = False
+        self.geschmiedet = False
+        self.abgegratet = False
+        self.getellert = False
+        self.versenkt = False
+        self.gebohrt = False
+        self.geoelt = False
+        self.vormontiert = False
+        self.vernietet = False
+        self.geoeffnet = False
+        self.eingestellt = False
+        self.geprueft = False
