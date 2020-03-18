edbb955809e63eae2be7d9993699681b793d534a
TimSchrick <62242406+TimSchrick@users.noreply.github.com>
2020-03-18 14:27:18 +0100

    delete

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from Gangeinstellen import Gangeinstellen
+from Zange import Zange
+
+
+if __name__ == '__main__':
+
+    zange = Zange()
+    pG = Gangeinstellen(zange)
+    zange = pG.getresult()
+    print(zange.geoeffnet)
